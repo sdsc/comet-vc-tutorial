f3021cebc2a01c87d02c066dcb8ad24f1016e5c8
Rick Wagner <rpwagner@vct01.sdsc.edu>
2016-07-14 00:30:55 -0700

    files moved

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,14 @@ apt-get install $APPS -y
 cd $HOME
 git clone https://github.com/sdsc/comet-vc-tutorial.git
 
+# Deploy config
+cp -rf $HOME/comet-vc-tutorial/config/etc/* /etc/
+cp -rf $HOME/comet-vc-tutorial/config/var/* /var/
+
 # configure the internal NIC
 echo '
+        pre-up iptables-restore < /etc/iptables.rules
+
 auto eth0
 iface eth0 inet static
         address 10.0.0.254
@@ -20,55 +26,11 @@ iface eth0 inet static
 ' >> /etc/network/interfaces
 ifup eth0
 
-# NAT
-sysctl -w net.ipv4.ip_forward=1
-echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-iptables -A FORWARD -i eth1 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+sysctl -p
 /etc/init.d/networking restart
-
-# DHCP
-apt-get install apache2 tftpd-hpa inetutils-inetd
-echo 'INTERFACES="eth0"' >> /etc/default/isc-dhcp-server
-service isc-dhcp-server restart
-
-echo '
-subnet 10.0.0.0 netmask 255.255.255.0 {
- range 10.0.0.100 10.0.0.200;
- option routers 10.0.0.254;
- option domain-name-servers 198.202.75.26;
-}
-' >> /etc/dhcp/dhcpd.conf
-
-# PXEbooting for compute nodes
-echo '
-allow booting;
-allow bootp;
-option option-128 code 128 = string;
-option option-129 code 129 = text;
-next-server 10.0.0.254;
-filename "pxelinux.0";
-' >> /etc/dhcp/dhcpd.conf
-
-echo '
-RUN_DAEMON="yes"
-OPTIONS="-l -s /var/lib/tftpboot"
-' >> /etc/default/tftpd-hpa
-echo '
-tftp    dgram   udp    wait    root    /usr/sbin/in.tftpd /usr/sbin/in.tftpd -s /var/lib/tftpboot
-' >> /etc/inetd.conf
+/etc/init.d/isc-dhcp-server restart
 /etc/init.d/tftpd-hpa restart
-
-# firewall rules update
-ufw allow 22/tcp
-ufw allow from 10.0.0.0/24
-ufw disable && ufw enable
-service ssh restart
-
-# Deploy config
-cp -rf $HOME/comet-vc-tutorial/config/etc/* /etc/
-cp -rf $HOME/comet-vc-tutorial/config/var/* /var/
+/etc/init.d/ ssh restart
 
 python cmutil.py pxefile $HOSTNAME
 python cmutil.py setkey
