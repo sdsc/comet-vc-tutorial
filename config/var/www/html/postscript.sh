a0145bc39b90a23a01b3809eccda7e70b1de4991
fugangwang <kevinwangfg@gmail.com>
2016-07-13 21:39:41 -0700

    updated kickstart and postscript

--- config/var/www/html/postscript.sh
+++ config/var/www/html/postscript.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 # Install ssh key
-cd /home/ubuntu
+cd /root
 mkdir --mode=700 .ssh
 cat >> .ssh/authorized_keys << "PUBLIC_KEY"
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfh9Fcu2scXguBqwuO1XHM5+FuMq7ZFPQK9PrDvKOARJpDlTTFDRF89vlLKeuVFYFA4gnK97MHE5hAIFmjmY9Q5AzPOyTbREaDVTDK/m2lndr4hCWusQJsSF0uMxAiI9fzsgzDquFVKUASKzUb/6FY5UqD8RjNOiwAmno10a3r/A0H8T9BEcDMhPWJfeTQbqtyL5DgBTV/kwO8KcNahLOBjBbwaiqPojeWsoFaVNOpS6vM2sVEpEAmgeeyQ8214eE51j/8tXIBGRExlmMW0owly1QzcRt78YkAO+dbO+Nl3+TLktbIZmLo5OLw1ze2kiecfBz4T8dxwuytc+qxjxK9 iucomet@vc4
+$PUBLICKEY
 PUBLIC_KEY
-chown -R ubuntu:ubuntu .ssh
 chmod 600 .ssh/authorized_keys
 sed -i '/home/d' /etc/fstab
 echo '192.168.1.254:/home /home nfs defaults 0 0' >> /etc/fstab
-sed -i 's/ens3/eth0/' /etc/network/interfaces
+#sed -i 's/ens3/eth0/' /etc/network/interfaces
 echo 'mlx4_ib
 ib_umad
 ib_ipoib
@@ -24,8 +23,8 @@ echo '
 #    network 10.27.0.0
 #    broadcast 10.27.0.255' >> /etc/network/interfaces
 
-sed -i '/^ubuntu/d' /etc/group
-echo 'ubuntu:x:1001:' >> /etc/group
+#sed -i '/^ubuntu/d' /etc/group
+#echo 'ubuntu:x:1001:' >> /etc/group
 
 echo 'btl_openib_warn_no_device_params_found = 0' >>  /etc/openmpi/openmpi-mca-params.conf
 
