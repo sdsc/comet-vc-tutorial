388b44f151c78c261203ec89aa08e251149b33be
fugangwang <kevinwangfg@gmail.com>
2016-07-14 10:37:44 -0400

    more updating of the pxeboot and deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,18 @@
 # Set up script for a basic Ubuntu 14.04 virtual cluster on Comet
 
 APPS="python-dev libffi-dev libssl-dev gcc git apache2 tftpd-hpa isc-dhcp-server inetutils-inetd"
-SERVICES="networking isc-dhcp-server tftpd-hpa inetutils-inetd ssh"
+SERVICES="networking isc-dhcp-server tftpd-hpa inetutils-inetd ssh nfs-kernel-server"
 
 apt-get install $APPS -y
 
 cd $HOME
 git clone https://github.com/sdsc/comet-vc-tutorial.git
+
+# get netboot files
+mount -t iso9660 /dev/cdrom /media/cdrom
+cp -r /media/cdrom/install/netboot/* /var/lib/tftpboot/
+chown -R nobody:nogroup /var/lib/tftpboot
+umount /media/cdrom
 
 # Deploy config
 cp -rf $HOME/comet-vc-tutorial/config/etc/* /etc/
@@ -33,19 +39,16 @@ do
     /etc/init.d/$i restart
 done
 
-# get netboot files
-mount -t iso9660 /dev/cdrom /media/cdrom
-cp -r /media/cdrom/install/netboot/* /var/lib/tftpboot/
-chown -R nobody:nogroup /var/lib/tftpboot
-umount /media/cdrom
 
 # install Cloudmesh
 $HOME/comet-vc-tutorial/setup-cloudmesh-client-ubuntu-trusty.sh
 
+# we have to run the script with sudo, or sudo su - before calling it,
+# thus, the $HOME would be /root
+#
 # clean up any permissions issues caused by Cloudmesh install as root
-chown -R $USER:$USER /home/$USER
+# chown -R $USER:$USER /home/$USER
 
 python cmutil.py pxefile $HOSTNAME
 python cmutil.py setkey
 python cmutil.py setpassword
-python cmutil.py setboot $HOSTNAME node1 net=true
